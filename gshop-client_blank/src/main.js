1a6545a8d1524c7400efb427601c4cce4a367fb5
programmerxzj <1638492106@qq.com>
2020-11-13 22:36:18 +0800

    提交

---
+++ gshop-client_blank/src/main.js
@@ -0,0 +1,14 @@
+/*
+* 入口js文件
+* */
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import store from './store'
+
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  router,
+  store
+})
