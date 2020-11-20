ec841825a92b6a5cc838d8b504816d0fe31d9273
programmerxzj <1638492106@qq.com>
2020-11-20 13:05:51 +0800

    rename

--- gshop-client_blank/src/mock/mockServer.js
+++
@@ -1,9 +0,0 @@
-import Mock from 'mockjs'
-import data from './data'
-
-//返回goods接口
-Mock.mock('/goods', {code: 0, data: data.goods})
-//返回goods接口
-Mock.mock('/info', {code: 0, data: data.info})
-//返回goods接口
-Mock.mock('/ratings', {code: 0, data: data.ratings})
